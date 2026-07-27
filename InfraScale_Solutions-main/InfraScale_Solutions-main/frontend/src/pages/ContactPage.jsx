7f1178a74da1f27f11d9c0026bff65d1fbce3b8f
Shreenivasa Murthy B V <179219653+shreenivasabv@users.noreply.github.com>
2026-07-27 10:58:52 +0530

    Delete InfraScale_Solutions-main/InfraScale_Solutions-main directory

--- InfraScale_Solutions-main/InfraScale_Solutions-main/frontend/src/pages/ContactPage.jsx
+++
@@ -1,9 +0,0 @@
-function ContactPage() {
-  return (
-    <div style={{ padding: "100px" }}>
-      <h1>Contact Page</h1>
-    </div>
-  );
-}
-
-export default ContactPage;
--- InfraScale_Solutions-main/InfraScale_Solutions-main/frontend/src/pages/StorageBackup.jsx
+++
@@ -1,9 +0,0 @@
-function StorageBackup() {
-  return (
-    <div style={{ padding: "100px" }}>
-      <h1>Storage & Backup Services</h1>
-    </div>
-  );
-}
-
-export default StorageBackup;
